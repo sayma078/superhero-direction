28ecefc4fa85033627dd031906099c792a6a9f45
sayma078 <somayasayma2@gmail.com>
2021-09-26 10:58:38 +0600

    added

---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className="header">
+            <h2>Coronavirus Outbreak:Latest Update</h2>
+            <h4>The United States leads the world in cases of COVID-19. We'll provide the latest updates on coronavirus cases, government response, impacts to our daily life, and more.</h4>
+            <h3>Total case: 219 Million</h3>
+        </div>
+    );
+};
+
+export default Header;
